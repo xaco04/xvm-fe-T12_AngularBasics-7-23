5d1b8e1ccdd1872356b3c5eb2be119b0dff51a4d
xaco04 <xavivico04@gmail.com>
2023-07-05 13:16:05 +0200

    calculadora acabada falten acabar els detalls

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -8,24 +8,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'calculadora';
   display: string = '';
+  operacion: string = '';
+  resultado: string = '';
 
-  addToDisplay(value: number) {
-    this.display += value.toString();
+  // metode per poder ficar els valors
+  addToDisplay(value: number | string) {
+    if (value === '.') {
+      this.operacion += value;
+    } else {
+      this.operacion += value.toString();
+    }
   }
-
+  // metode per poder ficar el operador 
   addOperator(operator: string) {
-    this.display += operator;
+    this.operacion += operator;
   }
-
+// metode per realitzar la operacio
   calculate() {
     try {
-      this.display = eval(this.display);
+      this.resultado = eval(this.operacion);
+      this.display = this.resultado;
     } catch (error) {
-      this.display = 'Error';
+      this.resultado = 'Error';
+      this.display = this.resultado;
     }
   }
 
   clear() {
     this.display = '';
+    this.operacion = '';
+    this.resultado = '';
   }
 }
